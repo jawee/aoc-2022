f862099ccf4ea944ace0961e9c985b85af636370
Andreas Olsson <olsson.andreas@gmail.com>
2022-12-03 15:50:14 +0100

    Restructured day1

--- dayone/b.go
+++ dayone/b.go
@@ -3,39 +3,12 @@ package dayone
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 	"sort"
 	"strconv"
 )
 
-func Day1A() {
-    pwd, _ := os.Getwd()
-    file, err := os.Open(pwd + "/dayone/input.txt")
-
-    if err != nil {
-        fmt.Println(err)
-    }
-
-    defer file.Close()
-
-    scanner := bufio.NewScanner(file)
-    max := 0
-    currSum := 0
-    for scanner.Scan() {
-        // fmt.Println(scanner.Text())
-        if scanner.Text() == "" {
-            max = int(math.Max((float64(max)), float64(currSum)))
-            currSum = 0
-        }
-        v, _ := strconv.Atoi(scanner.Text())
-        currSum = currSum + v
-    }
-    max = int(math.Max((float64(max)), float64(currSum)))
-    fmt.Println(max)
-}
-
-func Day1B() {
+func B() {
     pwd, _ := os.Getwd()
     file, err := os.Open(pwd + "/dayone/input.txt")
 
