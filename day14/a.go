7a871595e76ef8f550e65961c08476e28247cc5a
Andreas Olsson <olsson.andreas@gmail.com>
2022-12-21 08:20:01 +0000

    dont source control inputs

--- day14/a.go
+++ day14/a.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func A() {
@@ -19,11 +21,67 @@ func A() {
 
 	scanner := bufio.NewScanner(file)
 
+    rocks := make([]Rock, 0)
 	for scanner.Scan() {
 		l1 := scanner.Text()
+        rl := getRocksForInput(l1)
+        rocks = append(rocks, rl...)
 	}
 
-    sum := 0
+    sum := len(rocks)
 	fmt.Printf("%d\n", sum)
 }
 
+func getRocksForInput(l1 string) []Rock {
+    rl := make([]Rock, 0)
+    segments := strings.Split(l1, "->")
+    fx := -1
+    fy := -1
+    for _, s := range segments {
+        // fmt.Printf("%s\n", s)
+        coords := strings.Split(strings.Trim(s, " "), ",")
+        x, _ := strconv.Atoi(coords[0])
+        y, _ := strconv.Atoi(coords[1])
+
+        if fx == -1 && fy == -1 {
+            fx = x
+            fy = y
+            continue
+        }
+
+        if x == fx {
+            //handle y-movement
+            dir := 0
+            if y > fy {
+                dir = 1
+            } else {
+                dir = -1
+            }
+
+            for y != fy {
+                rl = append(rl, Rock{x,fy})
+                fy += dir
+            }
+        } else if y == fy {
+            //handle x-movement
+            dir := 0
+            if x > fx {
+                dir = 1
+            } else {
+                dir = -1
+            }
+
+            for x != fx {
+                rl = append(rl, Rock{fx,y})
+                fx += dir
+            }
+        }
+    }
+
+    return rl
+}
+
+type Rock struct {
+    x int
+    y int
+}
