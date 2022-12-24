596447ac45f68bc9b07580bdbfdc13cc15d124da
Andreas Olsson <olsson.andreas@gmail.com>
2022-12-24 12:36:26 +0100

    continued day14

--- day14/a.go
+++ day14/a.go
@@ -21,67 +21,148 @@ func A() {
 
 	scanner := bufio.NewScanner(file)
 
-    rocks := make([]Rock, 0)
+    objs := make(map[string]Obj)
+    // rocks := make([]Rock, 0)
 	for scanner.Scan() {
 		l1 := scanner.Text()
-        rl := getRocksForInput(l1)
-        rocks = append(rocks, rl...)
+        getRocksForInput(l1, objs)
 	}
 
-    sum := len(rocks)
+    sum := len(objs)
+    for k := range objs {
+        fmt.Println(k)
+    }
 	fmt.Printf("%d\n", sum)
 }
 
-func getRocksForInput(l1 string) []Rock {
-    rl := make([]Rock, 0)
+func dropSand(x int, rl map[string]Obj) {
+    fmt.Printf("Dropping sand from x: %d\n", x)
+    _ = dropSandRec(x, 0, rl)
+    fmt.Printf("dropSand:%v\n", rl)
+
+}
+
+func dropSandRec(x, y int, rl map[string]Obj) bool {
+    maxY := getMaxY(rl)
+    val, exists := rl[fmt.Sprintf("%d,%d", x, y+1)]
+    fmt.Printf("%v, %v\n", exists, val)
+    if y > maxY {
+        //falling to infinity
+        fmt.Printf("Falling to infinity\n")
+        return false
+    }
+    if exists && val.t == 1 {
+        fmt.Printf("Exists and is rock\n")
+        //rock hit
+        // check diagonally
+        // handle logic when rock is diagonally 
+        if x-1 >= 0 {
+            res := dropSandRec(x-1, y+1, rl)
+            if res {
+                return res
+            }
+        }
+        res := dropSandRec(x+1, y+1, rl)
+        if !res {
+            rl[fmt.Sprintf("%d,%d", x, y)] = Obj{t:2}
+        }
+        return res
+    }
+    if !exists {
+        if maxY < y+1 {
+            return false
+        }
+        fmt.Printf("Recurse\n")
+        return dropSandRec(x, y+1, rl)
+    }
+    // rl[fmt.Sprintf("%d,%d", x, y)] = Obj{t:1}
+    rl[fmt.Sprintf("%d,%d", x, y)] = Obj{t:2}
+    fmt.Printf("dropSandRec:%v\n", rl)
+    return true
+}
+
+func getMaxY(rl map[string]Obj) int {
+    max := 0
+
+    for k, _ := range rl {
+        p := strings.Split(k, ",")
+        y, _ := strconv.Atoi(p[1])
+        if y > max {
+            max = y
+        }
+    }
+
+    return max
+}
+
+func getRocksForInput(l1 string, rl map[string]Obj) {
     segments := strings.Split(l1, "->")
     fx := -1
     fy := -1
     for _, s := range segments {
-        // fmt.Printf("%s\n", s)
         coords := strings.Split(strings.Trim(s, " "), ",")
         x, _ := strconv.Atoi(coords[0])
         y, _ := strconv.Atoi(coords[1])
+        // fmt.Printf("Handle %d,%d %d,%d\n",x,y, fx,fy)
 
         if fx == -1 && fy == -1 {
             fx = x
             fy = y
+            // rl[fmt.Sprintf("%d,%d", x, y)] = Obj{1}
+            // fmt.Printf("continue %d,%d\n",x,y)
             continue
         }
 
         if x == fx {
+            // fmt.Printf("Handling y-movement\n")
             //handle y-movement
             dir := 0
             if y > fy {
                 dir = 1
+                for y >= fy {
+                    // fmt.Printf("Creating rock at %d,%d\n", x,fy)
+                    rl[fmt.Sprintf("%d,%d", x, fy)] = Obj{t:1}
+                    fy += dir
+                }
             } else {
                 dir = -1
+                for y <= fy {
+                    // fmt.Printf("Creating rock at %d,%d\n", x,fy)
+                    rl[fmt.Sprintf("%d,%d", x, fy)] = Obj{t:1}
+                    fy += dir
+                }
             }
 
-            for y != fy {
-                rl = append(rl, Rock{x,fy})
-                fy += dir
-            }
+            fx = x
+            fy = y
         } else if y == fy {
+            // fmt.Printf("Handling x-movement\n")
             //handle x-movement
             dir := 0
             if x > fx {
                 dir = 1
+                for x >= fx {
+                    // fmt.Printf("Creating rock at %d,%d\n", fx,y)
+                    rl[fmt.Sprintf("%d,%d", fx, y)] = Obj{t:1}
+                    fx += dir
+                }
             } else {
                 dir = -1
+                for x <= fx {
+                    // fmt.Printf("Creating rock at %d,%d\n", fx,y)
+                    rl[fmt.Sprintf("%d,%d", fx, y)] = Obj{t:1}
+                    fx += dir
+                }
             }
 
-            for x != fx {
-                rl = append(rl, Rock{fx,y})
-                fx += dir
-            }
+            fx = x
+            fy = y
         }
     }
 
-    return rl
+    // return rl
 }
 
-type Rock struct {
-    x int
-    y int
+type Obj struct {
+    t int
 }
---
+++ day14/a_test.go
@@ -0,0 +1,76 @@
+package day14
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDropSand(t *testing.T) {
+    cases := []struct {
+        input string
+        x int
+        n int
+        expected []string
+    }{
+        {"1,2 -> 3,2", 2, 1, []string{"1,2","2,2","3,2","2,1"}},
+        {"1,2 -> 3,2", 2, 2, []string{"1,2","2,2","3,2","2,1","1,1"}},
+        // {"1,2 -> 3,2", 2, 3, []string{"1,2","2,2","3,2","2,1","1,1","3,1"}},
+        // {"1,2 -> 3,2", 2, 4, []string{"1,2","2,2","3,2","2,1","1,1","3,1", "2,0"}},
+        // {"1,2 -> 3,2", 2, 5, []string{"1,2","2,2","3,2","2,1","1,1","3,1", "2,0"}},
+    }
+
+    for _, c := range cases {
+        fmt.Printf("#########################\n")
+        fmt.Printf("Testcase %s\n", c.input)
+        fmt.Printf("#########################\n")
+
+        m := make(map[string]Obj)
+        getRocksForInput(c.input, m)
+
+        for i := 0; i < c.n; i++ {
+            dropSand(c.x, m)
+        }
+
+        if len(m) != len(c.expected) {
+            t.Fatalf("Expected length %d, got %d\n", len(c.expected), len(m))
+        }
+        for _, e := range c.expected {
+            _, exists := m[e]
+            if !exists {
+                t.Fatalf("Expected obj at %s, got nil\n", e);
+            }
+        }
+
+    }
+}
+
+func TestGetRocks(t *testing.T) {
+    cases := []struct {
+        input string
+        expected []string
+    }{
+        {"1,1 -> 1,2", []string{"1,1","1,2"}},
+        {"1,1 -> 1,3", []string{"1,1","1,2","1,3"}},
+        {"1,1 -> 1,2 -> 2,2", []string{"1,1","1,2","2,2"}},
+        {"1,1 -> 1,3 -> 2,3", []string{"1,1","1,2","1,3","2,3"}},
+        {"3,3 -> 3,1 -> 1,1", []string{"3,3","3,2","3,1","2,1","1,1"}},
+    }
+
+    for _, c := range cases {
+        fmt.Printf("#########################\n")
+        fmt.Printf("Testcase %s\n", c.input)
+        fmt.Printf("#########################\n")
+        m := make(map[string]Obj)
+        getRocksForInput(c.input, m)
+
+        if len(m) != len(c.expected) {
+            t.Fatalf("Expected length %d, got %d\n", len(c.expected), len(m))
+        }
+        for _, e := range c.expected {
+            _, exists := m[e]
+            if !exists {
+                t.Fatalf("Expected rock at %s, got nil\n", e);
+            }
+        }
+    }
+}
